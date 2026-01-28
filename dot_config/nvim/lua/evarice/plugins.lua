68a4c8bcf0bb9b863601cc9d2f93a158f7d9b1d0
Brandon Lam <brandon.lam@rapsodo.com>
2026-01-28 10:04:38 +0800

    refactor: clean up nvim

--- dot_config/nvim/lua/evarice/plugins.lua
+++ dot_config/nvim/lua/evarice/plugins.lua
@@ -3,10 +3,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-Plug 'nvim-lua/plenary.nvim'
-Plug 'BurntSushi/ripgrep'
-Plug 'nvim-telescope/telescope-fzf-native.nvim'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-treesitter/nvim-treesitter'
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'make' })
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-treesitter/nvim-treesitter')
 
 vim.call('plug#end')
