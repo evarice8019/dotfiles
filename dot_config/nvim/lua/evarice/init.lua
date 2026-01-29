e77c442cce49dd19b4fcf01fcb393f365842f6ef
Brandon Lam <brandon.lam@rapsodo.com>
2026-01-29 14:10:49 +0800

    feat: configured nvim lsp

--- dot_config/nvim/lua/evarice/init.lua
+++ dot_config/nvim/lua/evarice/init.lua
@@ -3,5 +3,13 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- Show absolute line numbers
 vim.wo.number = true
 
+-- autocmds
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "c", "cpp" },
+    callback = function() vim.treesitter.start() end,
+})
+
+vim.lsp.enable('clangd')
+
 require("evarice.plugins")
 require("evarice.keymap")
--- dot_config/nvim/lua/evarice/keymap.lua
+++ dot_config/nvim/lua/evarice/keymap.lua
@@ -1,2 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local opts = { buffer = event.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+  end,
+})
