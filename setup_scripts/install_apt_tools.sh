7c312e560399cde695e01a3271a7c816d6b8ef29
Brandon Lam <35512190+evarice8019@users.noreply.github.com>
2026-01-31 01:12:15 +0800

    chezmoi: updated scripts

--- setup_scripts/install_apt_tools.sh
+++ setup_scripts/install_apt_tools.sh
@@ -4,3 +4,5 @@ sudo apt update
 sudo apt install bat eza fd-find git-delta ripgrep hyperfine -y
 sudo apt autoremove -y
 
+ln -s $(which fdfind) ~/.local/bin/fd
+ln -s $(which batcat) ~/.local/bin/bat
