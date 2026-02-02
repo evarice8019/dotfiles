16e0695378ab1e93c1c0aed995a4366358b91027
Brandon Lam <brandon.lam@rapsodo.com>
2026-02-02 09:17:15 +0800

    nvim: removed old files

--- dot_config/nvim/after/plugin/nerdtree.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n", "<leader>nt", "<cmd>NERDTreeToggle<CR>")
-vim.keymap.set("n", "<C-b>", "<cmd>NERDTreeToggle<CR>")
--- dot_config/nvim/after/plugin/treesitter.lua
+++
@@ -1,1 +0,0 @@
-require('nvim-treesitter').install({ 'rust', 'cpp' }):wait(300000) -- wait max. 5 minutes
