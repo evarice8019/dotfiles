4078b244933147855fcc628e49263f9090bc8599
Brandon Lam <35512190+evarice8019@users.noreply.github.com>
2026-06-12 11:27:42 +0800

    feat(wezterm): added domain and workspace to status bar

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,6 +5,19 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "Catppuccin Mocha" -- or macchiato, frappe, latte
 config.color_scheme = "Tokyo Night" -- or macchiato, frappe, latte
 
+wezterm.on("update-right-status", function(window, pane)
+	local workspace = window:active_workspace()
+	local domain = pane:get_domain_name()
+
+	local overrides = window:effective_config()
+	local fancy_bg = overrides.window_frame.active_titlebar_bg
+	window:set_right_status(wezterm.format({
+		{ Background = { Color = fancy_bg } },
+		{ Foreground = { Color = overrides.window_frame.active_titlebar_fg } },
+		{ Text = " Domain: " .. domain .. " | Workspace: " .. workspace .. " " },
+	}))
+end)
+
 config.keys = {
 	-- Vertical split (side-by-side)
 	{
