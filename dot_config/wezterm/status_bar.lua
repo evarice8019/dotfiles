a29dcb7532e38975610b9d76781f22d65dd25938
Brandon Lam <35512190+evarice8019@users.noreply.github.com>
2026-06-12 17:48:26 +0800

    feat(wezterm): added tmux session name to status bar

--- dot_config/wezterm/status_bar.lua
+++ dot_config/wezterm/status_bar.lua
@@ -3,8 +3,13 @@ local wezterm = require("wezterm")
 
 function M.setup()
 	wezterm.on("update-right-status", function(window, pane)
-		local workspace = window:active_workspace()
 		local domain = pane:get_domain_name()
+		local workspace = window:active_workspace()
+		local vars = pane:get_user_vars()
+		local tmux_session = vars.tmux_session
+		if tmux_session and tmux_session ~= "" then
+			workspace = tmux_session
+		end
 
 		local overrides = window:effective_config()
 		local fancy_bg = overrides.window_frame.active_titlebar_bg
