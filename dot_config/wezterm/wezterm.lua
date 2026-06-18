241a0fa5276482d7362aa808bea8b39287b75625
Brandon Lam <35512190+evarice8019@users.noreply.github.com>
2026-06-18 21:25:38 +0800

    feat(wezterm): changed theme to tokyonight-moon

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- config.color_scheme = "Catppuccin Mocha" -- or macchiato, frappe, latte
-config.color_scheme = "Tokyo Night" -- or macchiato, frappe, latte
+config.color_scheme = "tokyonight_moon" -- or macchiato, frappe, latte
 
 config.font = wezterm.font_with_fallback({
 	"JetBrainsMonoNL Nerd Font",
