eb1e179e86302b20dd48c213dfdaeff403c2038a
Brandon Lam <35512190+evarice8019@users.noreply.github.com>
2026-07-28 04:10:01 -0400

    fix: enabled clipboard through ssh

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,6 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set the clipboard provider to use OSC 52
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+vim.opt.clipboard = "unnamedplus" -- use system clipboard as default register
+
 vim.g.snacks_animate = false
 vim.opt.conceallevel = 0
 vim.opt.statuscolumn = "%s %{v:relnum} %{v:lnum}"
